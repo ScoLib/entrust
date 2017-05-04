7195b711fc46e64027caa17ce39f4e73b817567e
klgd <slice2008@qq.com>
2017-05-04 15:29:43 +0800

    remove user config

--- src/config/config.php
+++ src/config/config.php
@@ -46,28 +46,6 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Application User Model
-    |--------------------------------------------------------------------------
-    |
-    | This is the User model used by Entrust to create correct relations.
-    | Update the User if it is in a different namespace.
-    |
-    */
-    'user' => 'App\User',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Application Users Table
-    |--------------------------------------------------------------------------
-    |
-    | This is the users table used by the application to save users to the
-    | database.
-    |
-    */
-    'users_table' => 'users',
-
-    /*
-    |--------------------------------------------------------------------------
     | Entrust role_user Table
     |--------------------------------------------------------------------------
     |
